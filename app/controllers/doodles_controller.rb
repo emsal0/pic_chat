2a8d567533a818927f31d74c038d843be5ff8698
az <absolutezero2a03@gmail.com>
2014-03-10 09:31:49 -0700

    SUCCESS. Just need to do some CSS and alpha is complete.

--- app/controllers/doodles_controller.rb
+++ app/controllers/doodles_controller.rb
@@ -4,11 +4,12 @@ class DoodlesController < ApplicationController
 		img = Base64.decode64(imgurl['data:image/jpg;base64,'.length .. -1])
 		@doodle = Doodle.new
 		@doodle.save
-		@path = "app/assets/images/doodles/" + @doodle.id.to_s + '.jpg'
+		@path = "public/doodles/" + @doodle.id.to_s + '.jpg'
 		@doodle.path = @path
 		@doodle.save
 		File.open(@doodle.path, 'wb') { |f| f.write(img) }
-		redirect_to 'room#index'
+		render json: {:id => @doodle.id}
+		#redirect_to 'room#index'
 	end
 
 	def show
